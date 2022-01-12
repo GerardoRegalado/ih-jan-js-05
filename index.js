0ae755a64946bf93efae35bba6d20b79d08b22db
Gerardo Regalado <gerardo.regalado24@gmail.com>
2022-01-12 12:06:21 -0600

    acceso a propiedades

---
+++ index.js
@@ -0,0 +1,46 @@
+// OBJETOS
+// NO PRIMITIVO
+// ESTÁN COMPUESTOS POR PROPIEDADES Y VALORES
+
+const mexico = {
+	comida: "Tacos"  // Propiedad "comida" y valor "Tacos"
+}
+
+
+console.log(mexico)
+
+// A. ACCEDER A LAS PROPIEDADES
+
+// DOT NOTATION - NOTACIÓN DE PUNTO
+console.log(mexico.comida)
+
+// BRACKET NOTATION - NOTACIÓN POR CORCHETES
+console.log(mexico["comida"])
+
+const olympicRecords= {
+    athletics100men: "Justing  Gatling", 
+    athleticsLongJumpMen: "Mike Powel",
+    swimming200Men: "Michale Phelps",
+    "swimming 200 Women": "Ariarne Titmus"
+}
+
+console.log(olympicRecords.athletics100men);
+console.log(olympicRecords["athletics100men"]);
+console.log(olympicRecords["swimming 200 Women"]);
+
+// B. AGREGAR PROPIEDADES
+
+const refresco = {
+	nombre: "Coca cola",
+	color: "Oscuro",
+	precio: 12,
+	tamanos: [600, 1000]
+}
+
+refresco.envase = "Plástico"
+
+console.log("Objeto de refresco:", refresco)
+
+refresco["competidor"] = "Pepsi"
+
+console.log(refresco)
