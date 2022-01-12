c22970692439f24f221dcce4fcfd2cd49cc7c0e6
Gerardo Regalado <gerardo.regalado24@gmail.com>
2022-01-12 11:54:28 -0600

    refresco y olympic records

---
+++ index.js
@@ -0,0 +1,29 @@
+// OBJETOS
+// NO PRIMITIVO
+// ESTÁN COMPUESTOS POR PROPIEDADES Y VALORES
+
+const mexico = {
+	comida: "Tacos"  // Propiedad "comida" y valor "Tacos"
+}
+
+
+console.log(mexico)
+
+// A. ACCEDER A LAS PROPIEDADES
+
+// DOT NOTATION - NOTACIÓN DE PUNTO
+console.log(mexico.comida)
+
+// BRACKET NOTATION - NOTACIÓN POR CORCHETES
+console.log(mexico["comida"])
+
+const olympicRecords= {
+    athletics100men: "Justing  Gatling", 
+    athleticsLongJumpMen: "Mike Powel",
+    swimming200Men: "Michale Phelps",
+    "swimming 200 Women": "Ariarne Titmus"
+}
+
+console.log(olympicRecords.athletics100men);
+console.log(olympicRecords["athletics100men"]);
+console.log(olympicRecords["swimming 200 Women"]);
